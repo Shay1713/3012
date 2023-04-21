31da06f32707199b0fdaeb8a721176d7da7d2d16
Shay1713 <116598440+Shay1713@users.noreply.github.com>
2023-04-21 10:51:29 -0400

    Update LabCodePrint.m

--- LabCodePrint.m
+++ LabCodePrint.m
@@ -1,3 +1,8 @@
+clear all
+close all
+
+%% LAB 1
+
 %% QUESTION 1
 
 % table parameters & measurements
@@ -121,6 +126,8 @@ rlocus(sys);
 
 %%% QUESTION 8
 
+% Reference Table 2
+
 % Plugged values from first column of RH Table
 % into variables based on column 1 and row 
 % number and tried different gain values of K
@@ -145,6 +152,168 @@ rlocus(sys);
 % (-K*(4.532455538564416e-19)) - 0 ) / (-0.0051 + K * (2.554888831734579e-19))) ) / (( (0.001862137839356)*(-44.6969 - K *(6.705112750399810e-04)) 
 % - (1)*(-0.0051 + K * (2.554888831734579e-19)) ) / (0.001862137839)) );
 
-% Reference Table 2
+
+
+%% LAB 2
+
+%% QUESTION 1
+
+% Found TF in lab 1 
+
+
+
+%% QUESTION 2
+
+% ROUTH TABLE CRITERION
+% K > 1/G(s)
+% K*z > 0
+% Reference Table 3
+
+% chose (s-z) as (s-6.68) , to cancel out 
+% the pole in the right half of G(s)'s P-Z plot map
+% kept K the same at -6.705
+% ... in doing so, both conditons in R-H table are satisfied
+
+figure;
+rlocus(transfer_function);
+title('Transfer Function Lab1 Root Locus');
+
+
+
+%% QUESTION 3
+
+[b,a] = eqtflength(num,den);
+[z,p,k] = tf2zp(b,a);
+%{
+z = zeros = 
+   1.0e-07 *
+  0.000000001905180 + 0.259994244262245i
+  0.000000001905180 - 0.259994244262245i
+p = poles = 
+                   0
+  -6.686447109116259
+   6.684698705145661
+  -0.000113733868756
+k = gain = 
+    -6.705112750399810e-04   
+%}
+
+%tfH = 1*(s-6.686447109116259) * (10^(-7) *(0.000000001905180 + 0.259994244262245*i) 
+% *(0.000000001905180 - 0.259994244262245*i)) / (s *s* (s + 6.686447109116259) * 
+% (s - 6.684698705145661)*(s +0.000113733868756));
+
+numTF = -6.705*10^(-7) *((s -0.000000001905180 + 0.259994244262245*i) *(s - 0.000000001905180-0.259994244262245*i));
+numH = sym2poly(numTF);
+denTF = (s *s* (s + 6.68) *(s +0.0001137));
+denH = sym2poly(denTF);
+transfer_functionH = tf(numH,denH);
+%   -6.705e-07 s^2 + 2.555e-15 s - 4.532e-08
+%  ----------------------------------------
+%       s^4 + 6.68 s^3 + 0.0007595 s^2
+
+figure;
+pzmap(transfer_functionH);% plot poles\zeros
+title('Transfer FunctionH Pole-Zero MapH');
+
+[z,p,k] = tf2zp(numH,denH);
+%disp('poles');disp(p); % poles are ...
+%                   0
+%                   0
+%  -6.680000000000000
+%  -0.000113700000000
+%disp('zeros'); disp(z); % zeros are ...
+%  0.000000001905180 + 0.259994244262245i
+%  0.000000001905180 - 0.259994244262245i
+
+
+
+%% QUESTION 4 
+
+% Refernece Table 4
+
+figure;
+step(transfer_functionH); 
+title('Step Responce');xlabel('Time'); ylabel('Amplitude');
+
+
+
+%% QUESTION 5
+
+figure;
+rlocus(transfer_functionH);
+title('Transfer FunctionH Root Locus');
+
+
+
+%% QUESTION 6
+
+% Reference Table 5
+
+[b,a] = eqtflength(numH,denH);
+[z,p,k] = tf2zp(b,a);
+% z = 0.000000001905180 + 0.259994244262245i
+%     0.000000001905180 - 0.259994244262245i
+% p = -6.680000000000000
+%     -0.000113700000000
+% k = -6.705000000000000e-07
+
+numPD = (-6.705*10^(-7) *s^2 + 2.555*10^(-15) *s - 4.532* 10^(-8) )*(s + 5.764);
+numPDH = sym2poly(numPD);
+transfer_functionPD = tf(numPDH,denH);
+% -6.705e-07*s^3 + 3.865e-06*s^2 - 4.532e        -08 s + 2.612e-07
+% -----------------------------------------
+% s^4 + 6.68*s^3 + 0.0007595*s^2
+
+numPDK = numPD * (-6.705000000000000e-07); % complex so using same K as before -6.705000000000000e-07
+numPDHK = sym2poly(numPDK);
+transfer_functionPD = tf(numPDHK,denH); 
+%4.496e-13*s^3 + 2.591e-12*s^2 + 3.039e-14*s + 1.752e-13
+% --------------------------------------
+% s^4 + 6.68*s^3 + 0.0007595*s^2
+
+
+
+%% QUESTION 7
+
+closedLoopPDHK = transfer_functionPD / (1+ transfer_functionPD);
+figure;
+pzmap(closedLoopPDHK);
+figure;
+step(closedLoopPDHK);
+figure;
+rlocus(closedLoopPDHK);
+
+
+
+%% QUESTION 8
+
+[z,p,k] = tf2zp(numPDHK, denH);
+% z =
+%  -5.764000000000000 + 0.000000000000000i
+%   0.000000001905295 + 0.259983364350492i
+%   0.000000001905295 - 0.259983364350492i
+% p =
+%                    0
+%                    0
+%   -6.680000000000000
+%   -0.000113700000000
+% k =
+%      4.495702500000000e-13
+
+H = zpk([5.764000000000000 -0.000000001905295+0.259983364350492i ...
+    -0.000000001905295-0.259983364350492i],[ 6.680000000000000 0.000113700000000] , 4.495702500000000e-13);
+
+C = pidtune(H,'PID');
+% C =
+%             1          
+%  Kp + Ki * --- + Kd * s
+%             s          
+%  with Kp = -3.45e+08, Ki = -699, Kd = -4.25e+13
+
+
+
+
+
+
 
 
